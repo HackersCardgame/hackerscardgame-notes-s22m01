098790b3a8aa7fc19706a46597e9eb8b39609b00
Your Name <you@example.com>
2022-02-04 03:11:03 +0100

    ...

--- tools/changeRemoteToGit.sh
+++ tools/changeRemoteToGit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git remote set-url --push origin git@github.com:HackersCardgame/hackerscardgame-notes-s22m01.git
+git remote set-url --push origin git@github.com-hackerscardgame-notes:HackersCardgame/hackerscardgame-notes-s22m01.git
